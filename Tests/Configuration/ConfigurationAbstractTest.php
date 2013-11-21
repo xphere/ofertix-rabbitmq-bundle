d04fd3a0acb98342c3a9fe1ce0eee1188973f3a5
Berny Cantos <berny.cantos@ofertix.com>
2013-11-21 11:02:09 +0100

    abstract Configuration tests to its own directory

---
+++ Tests/Configuration/ConfigurationAbstractTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Ofertix\RabbitMqBundle\Tests\Configuration;
+
+use Ofertix\RabbitMqBundle\DependencyInjection\Configuration;
+use Symfony\Component\Config\Definition\Processor;
+
+abstract class ConfigurationAbstractTest extends \PHPUnit_Framework_TestCase
+{
+    protected function processConfig(array $config)
+    {
+        $configuration = $this->getConfiguration();
+        $processor = $this->getProcessor();
+
+        return $processor->processConfiguration($configuration, func_get_args());
+    }
+
+    protected function getConfiguration()
+    {
+        return new Configuration('ofertix_rabbitmq_configuration');
+    }
+
+    protected function getProcessor()
+    {
+        return new Processor();
+    }
+}
--- Tests/Configuration/ConnectionTest.php
+++ Tests/Configuration/ConnectionTest.php
@@ -1,11 +1,8 @@
 <?php
 
-namespace Ofertix\RabbitMqBundle\Tests;
+namespace Ofertix\RabbitMqBundle\Tests\Configuration;
 
-use Ofertix\RabbitMqBundle as Bundle;
-use Symfony\Component\Config\Definition\Processor;
-
-class ConfigurationTest extends \PHPUnit_Framework_TestCase
+class ConfigurationTest extends ConfigurationAbstractTest
 {
     static protected $connection_data = array(
         'default' => array(
@@ -80,23 +77,5 @@ class ConfigurationTest extends \PHPUnit_Framework_TestCase
 
         $this->assertArrayHasKey('connections', $result);
         $this->assertEquals($expected, $result['connections']);
-    }
-
-    protected function processConfig(array $config)
-    {
-        $configuration = $this->getConfiguration();
-        $processor = $this->getProcessor();
-
-        return $processor->processConfiguration($configuration, func_get_args());
-    }
-
-    protected function getConfiguration()
-    {
-        return new Bundle\DependencyInjection\Configuration('ofertix_rabbitmq_configuration');
-    }
-
-    protected function getProcessor()
-    {
-        return new Processor();
     }
 }
